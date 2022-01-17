8f01a48f31ed6cc8fcf1627bb166474681528d20
liujingtian <liujingtian@apusapps.com>
2022-01-17 17:57:50 +0800

    完成我的页面的编写

--- app/src/main/java/com/jingtian/mtdemo/Utils/SetFont.kt
+++ app/src/main/java/com/jingtian/mtdemo/Utils/SetFont.kt
@@ -14,6 +14,22 @@ class SetFont {
         }
         fun getScreenHeight(activity: Activity):Int = activity.resources.displayMetrics.heightPixels
         fun getScreenWidth(activity: Activity):Int = activity.resources.displayMetrics.widthPixels
+        fun px2dip(context:Context, pxValue:Float):Int {
+            val scale = context.resources.displayMetrics.density;
+            return (pxValue / scale + 0.5f).toInt()
+        }
+
+        /**
+         * 将dip或dp值转换成为px值，保证尺寸大小不变
+         *
+         * @param context
+         * @param dpValue
+         * @return
+         */
+        fun dip2px(context:Context,dpValue:Float):Int {
+            val scale = context.resources.displayMetrics.density;
+            return (dpValue * scale + 0.5f).toInt()
+        }
     }
 
 }
