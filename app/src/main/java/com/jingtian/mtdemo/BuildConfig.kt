e7aab3ba91e69fcb76df170c0e0061644563e7ad
jingtianer <44424338+jingtianer@users.noreply.github.com>
2022-01-21 19:06:16 +0800

    viewBinding（除了ViewHolder） BaseApplication中减少utils的初始化 activity转场动画 添加sharedElement的应用，首页搜索栏

--- app/src/main/java/com/jingtian/mtdemo/BuildConfig.kt
+++
@@ -1,9 +0,0 @@
-package com.jingtian.mtdemo
-
-class  BuildConfig {
-    companion object {
-        const val DEBUG = true
-        const val APPLICATION_ID = "MTDEMO JINTIANER"
-        const val BUILD_TYPE = "DEBUG"
-    }
-}
